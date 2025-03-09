57ca032a9918532e3ebff2563d2fc18228cdd544
Jim Burlison <jburlison@gmail.com>
2025-03-09 07:45:09 -0400

    Still cant get textures to work but fixed the button position.

--- atm_10_pack/src/main/java/net/reign/atm10_pmmo/client/ClientGuiEvents.java
+++ atm_10_pack/src/main/java/net/reign/atm10_pmmo/client/ClientGuiEvents.java
@@ -10,50 +10,47 @@ import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.client.event.ScreenEvent;
 import net.puffish.skillsmod.client.SkillsClientMod;
-/*
+
 import net.reign.atm10_pmmo.RPGMod;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.components.WidgetSprites;
-*/
+
 
 public class ClientGuiEvents {
-    
-    // Position of your button
-    private static final int BUTTON_X_POS = 275; // Adjust as needed
-    private static final int BUTTON_Y_POS = 5;   // Adjust as needed
 
-    /*
-    private static final int BUTTON_WIDTH = 15;
-    private static final int BUTTON_HEIGHT = 15;
+    private static final int BUTTON_WIDTH = 20;
+    private static final int BUTTON_HEIGHT = 18;
     
-    private static final ResourceLocation SKILLS_BUTTON_TEXTURE = ResourceLocation.parse(RPGMod.MODID + ":textures/gui/skills_button.png");
-    private static final ResourceLocation SKILLS_BUTTON_TEXTURE_PRESSED = ResourceLocation.parse(RPGMod.MODID + ":textures/gui/skills_button_pressed.png");
-    private static final ResourceLocation SKILLS_BUTTON_TEXTURE_DISABLED = ResourceLocation.parse(RPGMod.MODID + ":textures/gui/skills_button_disabled.png");
-    */
+    //private static final ResourceLocation SKILLS_BUTTON_TEXTURE = ResourceLocation.parse(RPGMod.MODID + ":textures/gui/skills_button.png");
+    //private static final ResourceLocation SKILLS_BUTTON_TEXTURE_PRESSED = ResourceLocation.parse(RPGMod.MODID + ":textures/gui/skills_button_pressed.png");
+    // private static final ResourceLocation SKILLS_BUTTON_TEXTURE_DISABLED = ResourceLocation.parse(RPGMod.MODID + ":textures/gui/skills_button_disabled.png");
+
     @OnlyIn(Dist.CLIENT)
     @SubscribeEvent
-    public void onGuiInit(ScreenEvent.Init.Pre event) {
-        if (event.getScreen() instanceof InventoryScreen) {
-            /*
+    public void onGuiInit(ScreenEvent.Init.Post event) {
+        if (event.getScreen() instanceof InventoryScreen inv) {
+       /*   
             // enabled, disabled, enabled focused, disabled focused
-            var button = new WidgetSprites(SKILLS_BUTTON_TEXTURE, SKILLS_BUTTON_TEXTURE_DISABLED, SKILLS_BUTTON_TEXTURE_PRESSED, SKILLS_BUTTON_TEXTURE_DISABLED);
+            var button = new WidgetSprites(SKILLS_BUTTON_TEXTURE, SKILLS_BUTTON_TEXTURE_PRESSED);
 
             // Add a button with a skills icon
             event.addListener(new ImageButton(
-                BUTTON_X_POS, BUTTON_Y_POS, BUTTON_HEIGHT, BUTTON_WIDTH,  // x, y, width, height
+                inv.getGuiLeft() + 150, inv.height / 2 - 22, BUTTON_WIDTH, BUTTON_HEIGHT,  // x, y, width, height
                 button,
                 (b) -> {
                     openPufferfishSkillsScreen();
                 },
                 Component.translatable("atm10_pmmo.skills_button.tooltip")
             ));
-             */
-
-             var button = Button.builder(Component.translatable("atm10_pmmo.skills_button.tooltip"), (b) -> {
+        
+            */
+  
+             var button = Button.builder(Component.literal("S"), (b) -> {
                 openPufferfishSkillsScreen();
              })
-             .pos(BUTTON_X_POS, BUTTON_Y_POS)
+             .pos(inv.getGuiLeft() + 150, inv.height / 2 - 22)
+             .size(BUTTON_WIDTH, BUTTON_HEIGHT)
              .build();
 
             event.addListener(button);
